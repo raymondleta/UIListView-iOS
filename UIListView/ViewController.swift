ba8f39215f767ffc21337199beef4a72d7c6afcd
raymond gitonga <raytosh95@gmail.com>
2020-01-01 00:20:28 +0300

    Initial Commit

--- UIListView/ViewController.swift
+++ UIListView/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let countries = ["Kenya", "Uganda", "Tanzania", "Rwanda", "Burundi", "Ethiopia", "Djibouti", "Eritrea"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countriesCell", for: indexPath)
+        
+        cell.textLabel?.text = countries[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
 
