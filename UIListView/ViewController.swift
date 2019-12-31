e2941d27187e9adeecd2bdbc25ae1f8e840c959a
raymond gitonga <raytosh95@gmail.com>
2019-12-31 22:51:30 +0300

    Initial Commit

---
+++ UIListView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIListView
+//
+//  Created by Tosh  on 12/31/19.
+//  Copyright © 2019 Tosh . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
